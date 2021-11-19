db6e71ae72a30c841ee61e11a8354dd9cb46f209
Han Zhang <Stephenzhang0713@outlook.com>
2021-11-19 15:18:41 +0800

    抽象工厂模式

---
+++ go-design-pattern/Abstrct-Factory/iSportsFactory.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+/**
+抽象工厂接口
+*/
+type iSportsFactory interface {
+	makeShoe() iShoe
+	makeShirt() iShirt
+}
+
+func getSportsFactory(brand string) (iSportsFactory, error) {
+	if brand == "adidas" {
+		return &adidas{}, nil
+	}
+
+	if brand == "nike" {
+		return &nike{}, nil
+	}
+
+	return nil, fmt.Errorf("wrong brand type passed")
+}
