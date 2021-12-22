8abfbfb0f612ce318f324a99c5373115522abb19
Han Zhang <Stephenzhang0713@outlook.com>
2021-12-22 16:08:08 +0800

    Go design pattern

---
+++ go-design-pattern/builder/builder.go
@@ -0,0 +1,70 @@
+package builder
+
+// Builder 是生成器接口
+type Builder interface {
+	Part1()
+	Part2()
+	Part3()
+}
+
+type Director struct {
+	builder Builder
+}
+
+// NewDirector ...
+func NewDirector(builder Builder) *Director {
+	return &Director{builder: builder}
+}
+
+// Construct Product
+func (d *Director) Construct() {
+	d.builder.Part1()
+	d.builder.Part2()
+	d.builder.Part3()
+}
+
+type Builder1 struct {
+	result string
+}
+
+func (b *Builder1) Part1() {
+	// TODO implement me
+	b.result += "1"
+}
+
+func (b *Builder1) Part2() {
+	// TODO implement me
+	b.result += "2"
+}
+
+func (b *Builder1) Part3() {
+	// TODO implement me
+	b.result += "3"
+}
+
+func (b *Builder1) GetResult() string {
+	return b.result
+}
+
+type Builder2 struct {
+	result int
+}
+
+func (b *Builder2) Part1() {
+	// TODO implement me
+	b.result += 1
+}
+
+func (b *Builder2) Part2() {
+	// TODO implement me
+	b.result += 2
+}
+
+func (b *Builder2) Part3() {
+	// TODO implement me
+	b.result += 3
+}
+
+func (b *Builder2) GetResult() int {
+	return b.result
+}
